cddf5384fbddbd705962942d07138aa655f3a0be
Jörg Thalheim <joerg@thalheim.io>
2022-09-07 10:38:18 +0200

    allow unfree packages by default

--- pkgs/top-level/config.nix
+++ pkgs/top-level/config.nix
@@ -216,7 +216,7 @@ let
 
     allowUnfree = mkOption {
       type = types.bool;
-      default = false;
+      default = true;
       # getEnv part is in check-meta.nix
       defaultText = literalExpression ''false || builtins.getEnv "NIXPKGS_ALLOW_UNFREE" == "1"'';
       description = ''
@@ -392,5 +392,4 @@ in
       mapAttrsToList (k: v: "undeclared Nixpkgs option set: config.${k}") config._undeclared or { }
     );
   };
-
 }
