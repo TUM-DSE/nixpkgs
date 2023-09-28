e291257f4ad2c95111ae1f40a9f0391363086874
Adam Stephens <adam@valkor.net>
2023-09-27 22:38:53 -0400

    zfs: 2.1.12 -> 2.1.13

--- pkgs/os-specific/linux/zfs/stable.nix
+++ pkgs/os-specific/linux/zfs/stable.nix
@@ -14,10 +14,13 @@ callPackage ./generic.nix args {
   # check the release notes for compatible kernels
   kernelCompatible =
     if stdenv'.isx86_64 || removeLinuxDRM
-    then kernel.kernelOlder "6.4"
+    then kernel.kernelOlder "6.6"
     else kernel.kernelOlder "6.2";
-  latestCompatibleLinuxPackages = linuxKernel.packages.linux_6_1;
+  latestCompatibleLinuxPackages = if stdenv'.isx86_64 || removeLinuxDRM
+    then linuxKernel.packages.linux_6_5
+    else linuxKernel.packages.linux_6_1;
   extraPatches = [
+    # applied in version 2.2.x
     (fetchpatch {
       name = "musl.patch";
       url = "https://github.com/openzfs/zfs/commit/1f19826c9ac85835cbde61a7439d9d1fefe43a4a.patch";
