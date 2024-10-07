ed88381125173ed3e0308ea600a74ff349c9098d
Jörg Thalheim <joerg@thalheim.io>
2024-10-07 12:54:21 +0200

    lib/systems/architectures: add sapphirerapids/emeraldrapids
    For reference: https://en.wikichip.org/wiki/intel/microarchitectures/sapphire_rapids
    
    https://www.phoronix.com/news/GCC-13-Initial-Emerald-Rapids
    https://www.phoronix.com/news/LLVM-Adds-Intel-Emerald-Rapids

--- pkgs/build-support/cc-wrapper/default.nix
+++ pkgs/build-support/cc-wrapper/default.nix
@@ -159,6 +159,8 @@ let
         tigerlake      = versionAtLeast ccVersion "10.0";
         knm            = versionAtLeast ccVersion "8.0";
         alderlake      = versionAtLeast ccVersion "12.0";
+        sapphirerapids = versionAtLeast ccVersion "11.0";
+        emeraldrapids  = versionAtLeast ccVersion "13.0";
 
         # AMD
         znver1         = versionAtLeast ccVersion "6.0";
@@ -178,6 +180,8 @@ let
         icelake-server = versionAtLeast ccVersion "7.0";
         knm            = versionAtLeast ccVersion "7.0";
         alderlake      = versionAtLeast ccVersion "16.0";
+        sapphirerapids = versionAtLeast ccVersion "12.0";
+        emeraldrapids  = versionAtLeast ccVersion "16.0";
 
         # AMD
         znver1         = versionAtLeast ccVersion "4.0";
