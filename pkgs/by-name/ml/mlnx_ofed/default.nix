a97ac2a72ee008096bf03b383f1a62dd618f04dc
Teofil Bodea <teo.bodea12@yahoo.com>
2026-03-17 14:48:14 +0000

    Autodiscover modules

--- pkgs/by-name/ml/mlnx_ofed/default.nix
+++ pkgs/by-name/ml/mlnx_ofed/default.nix
@@ -80,16 +80,10 @@ in
       cp -r include $out/lib/modules/${kernel.modDirVersion}/extra/mlnx-ofa_kernel
 
         # Give mlnx-ofa_kernel modules priority over in-tree drivers
-  install -Dm644 /dev/stdin \
-    $out/lib/depmod.d/mlnx-ofa_kernel.conf << EOF
-  override mlx5_core * extra/mlnx-ofa_kernel
-  override mlx5_ib * extra/mlnx-ofa_kernel
-  override ib_core * extra/mlnx-ofa_kernel
-  override ib_uverbs * extra/mlnx-ofa_kernel
-  override rdma_cm * extra/mlnx-ofa_kernel
-  override iw_cm * extra/mlnx-ofa_kernel
-  override ib_cm * extra/mlnx-ofa_kernel
-  override ib_umad * extra/mlnx-ofa_kernel
-  EOF
+        install -dm644 $out/lib/depmod.d
+        for mod in $(find $out/lib/modules/${kernel.modDirVersion}/extra/mlnx-ofa_kernel -name "*.ko"); do
+          echo "override $(modinfo -F name "$mod") * extra/mlnx-ofa_kernel" \
+            >> $out/lib/depmod.d/mlnx-ofa_kernel.conf
+        done
     '';
   }
